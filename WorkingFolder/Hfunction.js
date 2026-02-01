524305c338fec346a8efccd7f14235fdb3cdbd3b
Aminat <ahmedaminat322@gmail.com>
2026-02-01 13:59:09 +0100

    added Hfunction

---
+++ WorkingFolder/Hfunction.js
@@ -0,0 +1,6 @@
+function greet(name, age, country){
+    return ` Hi! nice meeting you my name is ${name} i am ${age} years old from ${country}.`
+
+}
+let meeting = greet("Bola", 30, "FRN");
+console.log(meeting);
